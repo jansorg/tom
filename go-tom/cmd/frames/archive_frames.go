7ebbdf207c7155dfa086f057940f81035f7acf18
jansorg <github@joachim-ansorg.de>
2019-02-09 18:21:56 +0100

    support subprojects

--- go-tom/cmd/frames/archive_frames.go
+++ go-tom/cmd/frames/archive_frames.go
@@ -12,12 +12,13 @@ import (
 func newArchiveCommand(ctx *context.TomContext, parent *cobra.Command) *cobra.Command {
 	projectIDOrName := ""
 	nameDelimiter := ""
+	includeSubprojects := false
 
 	var cmd = &cobra.Command{
 		Use:   "archive",
 		Short: "Archive a set of frames",
 		Run: func(cmd *cobra.Command, args []string) {
-			if err := archiveFrames(projectIDOrName, nameDelimiter, ctx); err != nil {
+			if err := archiveFrames(projectIDOrName, nameDelimiter, includeSubprojects, ctx); err != nil {
 				util.Fatalf("Error archiving frames: %s", err.Error())
 			} else {
 				fmt.Println("archived project frames")
@@ -27,12 +28,13 @@ func newArchiveCommand(ctx *context.TomContext, parent *cobra.Command) *cobra.Co
 
 	cmd.Flags().StringVarP(&projectIDOrName, "project", "p", "", "Only frames of this project will be archived")
 	cmd.Flags().StringVarP(&nameDelimiter, "name-delimiter", "", "/", "Delimiter used in the full project name")
+	cmd.Flags().BoolVarP(&includeSubprojects, "include-subprojects", "", includeSubprojects, "Also archive frames of all subprojects")
 
 	parent.AddCommand(cmd)
 	return cmd
 }
 
-func archiveFrames(projectIDOrName string, nameDelimiter string, ctx *context.TomContext) error {
+func archiveFrames(projectIDOrName string, nameDelimiter string, includeSubprojects bool, ctx *context.TomContext) error {
 	ctx.Store.StartBatch()
 	defer ctx.Store.StopBatch()
 
@@ -41,7 +43,7 @@ func archiveFrames(projectIDOrName string, nameDelimiter string, ctx *context.To
 		return err
 	}
 
-	frames := ctx.Query.FramesByProject(project.ID, false)
+	frames := ctx.Query.FramesByProject(project.ID, includeSubprojects)
 	frames.ExcludeArchived()
 
 	for _, f := range frames {
--- go-tom/cmd/frames/archive_frames_test.go
+++ go-tom/cmd/frames/archive_frames_test.go
@@ -41,7 +41,7 @@ func TestArchiveCommand(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	err = archiveFrames(p1.GetFullName("/"), "/", ctx)
+	err = archiveFrames(p1.GetFullName("/"), "/", false, ctx)
 	require.NoError(t, err)
 
 	f1New, err := ctx.Query.FrameByID(f1.ID)
@@ -53,8 +53,50 @@ func TestArchiveCommand(t *testing.T) {
 	assert.False(t, f2New.Archived)
 
 	// now archive frame of subproject p2
-	err = archiveFrames(p2.ID, "/", ctx)
+	err = archiveFrames(p2.ID, "/", false, ctx)
 	f2New, err = ctx.Query.FrameByID(f2.ID)
 	require.NoError(t, err)
+	assert.True(t, f2New.Archived)
+}
+
+func TestArchiveCommandSubprojects(t *testing.T) {
+	ctx, err := test_setup.CreateTestContext(language.English)
+	require.NoError(t, err)
+	defer test_setup.CleanupTestContext(ctx)
+
+	p1, _, err := ctx.StoreHelper.GetOrCreateNestedProjectNames("project")
+	require.NoError(t, err)
+
+	p2, _, err := ctx.StoreHelper.GetOrCreateNestedProjectNames("project", "subproject")
+	require.NoError(t, err)
+
+	now := time.Now()
+	end := now.Add(10 * time.Minute)
+	f1, err := ctx.Store.AddFrame(model.Frame{
+		Start:     &now,
+		End:       &end,
+		ProjectId: p1.ID,
+	})
+	require.NoError(t, err)
+
+	now = time.Now()
+	end = now.Add(10 * time.Minute)
+	f2, err := ctx.Store.AddFrame(model.Frame{
+		Start:     &now,
+		End:       &end,
+		ProjectId: p2.ID,
+	})
+	require.NoError(t, err)
+
+	err = archiveFrames(p1.GetFullName("/"), "/", true, ctx)
+	require.NoError(t, err)
+
+	f1New, err := ctx.Query.FrameByID(f2.ID)
+	require.NoError(t, err)
+
+	f2New, err := ctx.Query.FrameByID(f2.ID)
+	require.NoError(t, err)
+
+	assert.True(t, f1New.Archived)
 	assert.True(t, f2New.Archived)
 }
