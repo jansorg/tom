5ce5e8e9bac724fadaccc7663f46c1165fb094ad
jansorg <github@joachim-ansorg.de>
2019-02-16 15:14:13 +0100

    update before release

--- release.bash
+++ release.bash
@@ -6,5 +6,7 @@ VERSION="$1"
 [[ -z "$VERSION" ]] && echo "No version defined" && exit -1
 
 bash ./update_docs.bash
+git comit -m "updating documentation for release of v$VERSION"
+
 git tag "v$VERSION"
 goreleaser --rm-dist
