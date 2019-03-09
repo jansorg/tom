71226055f2955eecdba78195303e1266a5426b84
jansorg <github@joachim-ansorg.de>
2019-03-09 19:01:27 +0100

    updating completions script for release of v0.7.1

--- release.bash
+++ release.bash
@@ -6,9 +6,9 @@ VERSION="$1"
 [[ -z "$VERSION" ]] && echo "No version defined" && exit -1
 
 (go build . && ./tom completion > scripts/completions/tom.sh && chmod u+x scripts/completions/tom.sh)
-git commit -m "updating completions script for release of v$VERSION" scrips/completions/ && git push
+git commit -m "updating completions script for release of v$VERSION" . && git push
 bash ./update_docs.bash
-git commit -m "updating documentation for release of v$VERSION" && git push
+git commit -m "updating documentation for release of v$VERSION" . && git push
 
 git tag "v$VERSION"
 goreleaser --rm-dist
