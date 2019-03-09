0855e5a93cc85da2a479e7236096db07ae37b7b8
jansorg <github@joachim-ansorg.de>
2019-03-09 19:07:03 +0100

    prepare release

--- release.bash
+++ release.bash
@@ -6,14 +6,14 @@ VERSION="$1"
 [[ -z "$VERSION" ]] && echo "No version defined" && exit -1
 
 (go build . && ./tom completion > scripts/completions/tom.sh && chmod u+x scripts/completions/tom.sh)
-git commit -m "updating completions script for release of v$VERSION" . && git push
+git commit -m "prep: updating completions script for release of v$VERSION" . && git push
 
 bash ./update_docs.bash
-git commit -m "updating documentation for release of v$VERSION" . && git push
+git commit -m "prep: updating documentation for release of v$VERSION" . && git push
 
 # gotext generates data in unstable order, we can't use go generate here because it's called by goreleaser
 gotext -srclang=en update -out=catalog.go -lang=en,de
-git commit -m "updating translations for release of v$VERSION" . && git push
+git commit -m "prep: updating translations for release of v$VERSION" . && git push
 
 git tag "v$VERSION"
 goreleaser --rm-dist
