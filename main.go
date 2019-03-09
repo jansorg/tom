b99432fdac87f0e797ed8ecb1e10d1b357be5112
jansorg <github@joachim-ansorg.de>
2019-03-09 19:05:08 +0100

    prepare release

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ var version = "unknown"
 var commit = "unknown"
 var date = "unknown"
 
-//go:generate gotext -srclang=en update -out=catalog.go -lang=en,de
 //go:generate go-bindata -pkg tom -prefix "templates/" -o go-tom/templates.go templates/...
 func main() {
 	cmd.Execute(version, commit, date)
--- release.bash
+++ release.bash
@@ -7,8 +7,13 @@ VERSION="$1"
 
 (go build . && ./tom completion > scripts/completions/tom.sh && chmod u+x scripts/completions/tom.sh)
 git commit -m "updating completions script for release of v$VERSION" . && git push
+
 bash ./update_docs.bash
 git commit -m "updating documentation for release of v$VERSION" . && git push
+
+# gotext generates data in unstable order, we can't use go generate here because it's called by goreleaser
+go generate gotext -srclang=en update -out=catalog.go -lang=en,de
+git commit -m "updating translations for release of v$VERSION" . && git push
 
 git tag "v$VERSION"
 goreleaser --rm-dist
